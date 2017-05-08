aa8f77e95ae15c493387315649f12fc34b1848b1
Jeff Hanson <hanson@uber.com>
2017-05-08 12:30:04 -0700

    update linkedin link

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
       {icon}
       <div className='App__links'>
         <Link href='https://github.com/jshanson7'>github</Link>
-        <Link href='https://www.linkedin.com/in/jeff-hanson-24b83522/'>linkedin</Link>
+        <Link href='https://www.linkedin.com/in/jshanson7/'>linkedin</Link>
         <Link href={`${PUBLIC_URL}/resume.pdf`}>resume</Link>
         <Link href='https://www.smartyplantsapp.com/'>latest project</Link>
       </div>
